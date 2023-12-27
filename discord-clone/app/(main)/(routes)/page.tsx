4642bee6aa4a96acb482597e08dcaaca6174cb2c
Kevin Lin <2004klin@gmail.com>
2023-12-27 01:41:32 -0500

    Setup folders

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -4,12 +4,7 @@ export default function Home() {
   return (
 
     <div className="flex flex-col">
-      <p className="text-3xl font-bold text-indigo-500">
-        Hello 
-      </p>
-      <Button>
-        Click Me
-      </Button>
+      This is a protected route.
     </div>
     
   )
